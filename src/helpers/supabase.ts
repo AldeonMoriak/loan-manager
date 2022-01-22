a96eeadd8d675fb3fb5e1dd4494f5d36e58287c6
Aldeon Arka <aldeonmoriak@gmail.com>
2022-01-22 09:45:03 +0330

    add autorefresh to the supabase instance

--- src/helpers/supabase.ts
+++ src/helpers/supabase.ts
@@ -1,7 +1,13 @@
-import { createClient } from '@supabase/supabase-js'
+import { createClient, SupabaseClientOptions } from '@supabase/supabase-js'
+
+const options: SupabaseClientOptions = {
+  schema: 'public',
+  autoRefreshToken: true,
+  detectSessionInUrl: true,
+}
 
 const supabaseUrl = import.meta.env.VITE_SUPABASE_URL as string;
 const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY as string;
 
 
-export const supabase = createClient(supabaseUrl, supabaseAnonKey)
+export const supabase = createClient(supabaseUrl, supabaseAnonKey, options)
